42578272b27c36cff82ba8b971d7e52091ea2924
Matheus Santos <matheus.corp.santos@gmail.com>
2021-11-07 02:30:17 -0300

    start projet

---
+++ app.js
@@ -0,0 +1,52 @@
+const src = "?SRC=";
+let getForm = document.querySelector(".formData");
+let postLink = document.querySelector("#links");
+
+getForm.addEventListener('submit', appendLink);
+postLink.addEventListener('click', copyNote);
+
+
+
+
+
+
+
+function  appendLink(e){
+    e.preventDefault();
+ 
+    let linkValue = document.querySelector(".newLink").value;
+    let linkSRC = document.querySelector(".newSRC").value;
+    let linkDate = document.querySelector(".date").value;
+
+    const linkGen = document.createElement('li');
+    linkGen.className ='dataContainter';
+    postLink.appendChild(linkGen);
+  
+    const linkData = document.createElement('p');
+    linkData.className ='finalLink';
+    linkData.appendChild(document.createTextNode(linkValue + src + linkSRC));
+    linkGen.appendChild(linkData);
+
+    const  dateData = document.createElement('p');
+    dateData.appendChild(document.createTextNode(linkDate));
+    linkGen.appendChild(dateData);
+
+
+    const copyBtn = document.createElement('button');
+    copyBtn.appendChild(document.createTextNode('Copiar'));
+    copyBtn.className='copyBtn';
+    linkGen.appendChild(copyBtn);
+
+}
+
+
+function copyNote(e) {
+    if(e.target.classList.contains('copyBtn')){        
+       let linkValueCopy = e.target.parentElement;
+        navigator.clipboard.writeText(linkValueCopy.querySelector('.finalLink').innerText);
+    }
+}
+
+
+
+
